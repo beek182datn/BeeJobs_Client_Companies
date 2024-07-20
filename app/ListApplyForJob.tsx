31caae8a80bbc00227505762a81a5872494a4744
DongBeer <dongnguyen2802nb@gmail.com>
2024-07-20 17:53:28 +0700

    add method edit stastus for applyjob

--- app/ListApplyForJob.tsx
+++ app/ListApplyForJob.tsx
@@ -1,92 +1,203 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useCallback  } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Modal, Alert, SafeAreaView } from 'react-native';
 import axios from 'axios';
-import { useLocalSearchParams } from 'expo-router';
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 const ListApplyForJob = () => {
-  const [applications, setApplications] = useState([]);
+  const [applicationsPending, setApplicationsPending] = useState([]);
+  const [applicationsReviewed, setApplicationsReviewed] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [noApplications, setNoApplications] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [applyJobId, setApplyJobId] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'pending', title: 'Chưa đánh giá' },
+    { key: 'reviewed', title: 'Đã đánh giá' },
+  ]);
 
-  const { jobId } = useLocalSearchParams(); 
+  const getStatusStyle = (status) => {
+    switch (status) {
+      case 'Đã xem':
+        return styles.statusReviewed;
+      case 'Phù hợp':
+        return styles.statusSuitable;
+      case 'Chưa phù hợp':
+        return styles.statusNotSuitable;
+      case 'Pending':
+        return styles.statusPending;
+      default:
+        return styles.statusValue; 
+    }
+  };
+
+  const { jobId } = useLocalSearchParams();
   const router = useRouter();
 
+  const fetchApplications = useCallback(async () => {
+    try {
+      const response = await axios.get(`http://beejobs.io.vn:14307/api/applyJobs/getApylyJobsByIdJob/${jobId}`);
+      if (response.status === 200 && response.data.data.length > 0) {
+        const pendingApplications = response.data.data.filter(app => app.status === 'pending');
+        const reviewedApplications = response.data.data.filter(app => app.status !== 'pending');
+        setApplicationsPending(pendingApplications);
+        setApplicationsReviewed(reviewedApplications);
+      }
+    } catch (error) {
+      console.error('Error fetching applications:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, [jobId]);
 
   useEffect(() => {
-    const fetchApplications = async () => {
-      try {
-        const response = await axios.get(`http://beejobs.io.vn:14307/api/applyJobs/getApylyJobsByIdJob/${jobId}`);
-        if (response.status === 200 && response.data.data.length > 0) {
-          setApplications(response.data.data);
-        } else {
-          setNoApplications(true);
-        }
-      } catch (error) {
-        if (error.response && error.response.status === 404) {
-          setNoApplications(true);
-        } else {
-          console.error('Error fetching applications:', error);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
+    setLoading(true); // Đặt loading trước khi fetch
     fetchApplications();
-  }, [jobId]);
+  }, [jobId, fetchApplications]);
 
   const handleViewProfile = (cv) => {
     console.log(`View profile of worker ${cv}`);
     router.push({ pathname: "ReadCv", params: { cvUrl: "http://beejobs.io.vn:14307" + cv } });
   };
 
-  const handleRateWorker = (workerId) => {
-    // Logic to navigate to rating screen or perform rating action
-    console.log(`Rate worker ${workerId}`);
+  const handleRateWorker = (_id) => {
+    setApplyJobId(_id);
+    setModalVisible(true);
+  };
+
+  const handleOptionSelect = async (option) => {
+    setModalVisible(false);
+    if (option === 'Cancel') return;
+
+    try {
+      const response = await axios.put(`http://beejobs.io.vn:14307/api/applyJobs/editApplyJobStatus/${applyJobId}`, {
+        status: option,
+      });
+      if (response.status === 200) {
+        Alert.alert('Thông báo', 'Đánh giá thành công');
+        fetchApplications();
+      }
+    } catch (error) {
+      console.error('Error rating worker:', error);
+      Alert.alert('Lỗi', 'Không thể đánh giá ứng viên');
+    }
   };
 
   const renderItem = ({ item }) => (
     <View style={styles.itemContainer}>
-      <Text style={styles.workerName}>Tên ứng viên: {item.worker_name}</Text>
-      <Text style={styles.phone}>Số điện thoại: {item.phone}</Text>
-      <Text style={styles.status}>Trạng thái: {item.status}</Text>
+      <Text style={styles.workerName}>Tên ứng viên: {item.fullname}</Text>
+      <Text style={styles.phone}>Số điện thoại: {item.phone_number}</Text>
+      <View style={{ flexDirection: 'row' }}>
+      <Text style={styles.statusLabel}>Trạng thái: </Text>
+      <Text style={[styles.statusValue, getStatusStyle(item.status)]}>
+        {item.status}
+      </Text>
+    </View>
       <Text style={styles.appliedAt}>Ứng tuyển lúc: {new Date(item.applied_at).toLocaleDateString()}</Text>
 
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button1} onPress={() => handleViewProfile("http://beejobs.io.vn:14307"+item.cv)}>
+        <TouchableOpacity style={styles.button1} onPress={() => handleViewProfile(item.cv)}>
           <Text style={styles.buttonText}>Xem hồ sơ</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleRateWorker(item.worker_id)}>
+        <TouchableOpacity style={styles.button} onPress={() => handleRateWorker(item._id)}>
           <Text style={styles.buttonText}>Đánh giá</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 
-  if (loading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
-
-  if (noApplications) {
-    return (
-      <View style={styles.noApplicationsContainer}>
-        <Text>Chưa có người ứng tuyển</Text>
-      </View>
-    );
-  }
+  const renderScene = SceneMap({
+    pending: () => (
+      loading ? (
+        <SafeAreaView style={styles.loadingContainer}>
+          <Text>Loading...</Text>
+        </SafeAreaView>
+      ) : applicationsPending.length === 0 ? (
+        <View style={styles.noApplicationsContainer}>
+          <Text>Chưa có hồ sơ ứng tuyển</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={applicationsPending}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id}
+          contentContainerStyle={styles.listContainer}
+        />
+      )
+    ),
+    reviewed: () => (
+      loading ? (
+        <View style={styles.loadingContainer}>
+          <Text>Loading...</Text>
+        </View>
+      ) : applicationsReviewed.length === 0 ? (
+        <View style={styles.noApplicationsContainer}>
+          <Text>Chưa có hồ sơ ứng tuyển</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={applicationsReviewed}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id}
+          contentContainerStyle={styles.listContainer}
+        />
+      )
+    ),
+  });
 
   return (
-    <FlatList
-      data={applications}
-      renderItem={renderItem}
-      keyExtractor={(item) => item._id}
-      contentContainerStyle={styles.listContainer}
-    />
+    <View style={{ flex: 1 }}>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            indicatorStyle={styles.tabIndicator}
+            style={styles.tabBar}
+            labelStyle={styles.tabLabel}
+          />
+        )}
+        style={{ flex: 1 }}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Chọn đánh giá</Text>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={() => handleOptionSelect('Đã xem')}
+            >
+              <Text style={styles.modalButtonText}>Đã xem</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalSuccess}
+              onPress={() => handleOptionSelect('Phù hợp')}
+            >
+              <Text style={styles.modalButtonText}>Phù hợp</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalFalse}
+              onPress={() => handleOptionSelect('Chưa phù hợp')}
+            >
+              <Text style={styles.modalButtonText}>Chưa phù hợp</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalCancle}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.modalButtonText}>Hủy</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 };
 
@@ -123,9 +234,25 @@ const styles = StyleSheet.create({
   phone: {
     fontSize: 16,
   },
-  status: {
-    fontSize: 14,
-    color: 'gray',
+  statusLabel: {
+    marginTop:5,
+    fontSize: 14, 
+  },
+  statusValue: {
+    fontSize: 18, 
+    fontWeight: 'bold', 
+  },
+  statusPending: {
+    color: '#FFA500', 
+  },
+  statusReviewed: {
+    color: '#0000FF', 
+  },
+  statusSuitable: {
+    color: '#5BBD2B', 
+  },
+  statusNotSuitable: {
+    color: '#e12828', 
   },
   appliedAt: {
     fontSize: 14,
@@ -152,9 +279,74 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
   },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContainer: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  modalButton: {
+    width: '100%',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#0099FF',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  modalSuccess: {
+    width: '100%',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#5BBD2B',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  modalFalse: {
+    width: '100%',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#e12828',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  modalCancle: {
+    width: '100%',
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: '#555555',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  modalButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  tabBar: {
+    marginTop:20,
+    backgroundColor: '#0099FF',
+    elevation: 4,
+    borderTopLeftRadius:15,
+    borderTopRightRadius:15,
+  },
+  tabLabel: {
+    fontSize: 16,
+    fontWeight:'bold'
+  },
+  tabIndicator: {
+    backgroundColor: '#fff',
+  },
 });
 
 export default ListApplyForJob;
-
-
-
